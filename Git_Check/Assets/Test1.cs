72e5d2770a16b18ec98ad938e894518f0ab12211
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2023-12-18 17:31:00 +0900

    sang

---
+++ Git_Check/Assets/Test1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("±ËªÛ¡ÿ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
